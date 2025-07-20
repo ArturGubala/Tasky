378bef52a15e1882f9aa6f7f4475d504910947f1
arturgie <gubala-artur@wp.pl>
2025-07-20 13:56:06 +0200

    Add: DataError interface to represent network and local errors; general Error and Result interfaces for handle responses.

---
+++ app/src/main/java/com/example/tasky/core/domain/util/DataError.kt
@@ -0,0 +1,20 @@
+package com.example.tasky.core.domain.util
+
+sealed interface DataError: Error {
+    enum class Network: DataError {
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        BAD_REQUEST,
+        CONFLICT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL
+    }
+}

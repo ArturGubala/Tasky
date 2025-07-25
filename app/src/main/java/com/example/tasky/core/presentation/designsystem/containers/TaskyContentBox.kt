d2440a60db1db71eceb03f589da4c3c9a49fc66e
arturgie <gubala-artur@wp.pl>
2025-07-25 23:22:28 +0200

    Add TaskyContentBox composable.

---
+++ app/src/main/java/com/example/tasky/core/presentation/designsystem/containers/TaskyContentBox.kt
@@ -0,0 +1,58 @@
+package com.example.tasky.core.presentation.designsystem.containers
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.tasky.core.presentation.designsystem.theme.TaskyTheme
+
+@Composable
+fun TaskyContentBox(
+    content: @Composable (() -> Unit),
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.CenterStart
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+            .background(MaterialTheme.colorScheme.surface),
+        contentAlignment = contentAlignment
+    ) {
+        content.invoke()
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun TaskyContentBoxPreview() {
+    TaskyTheme {
+        TaskyContentBox(
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp, top = 28.dp)
+                ) {
+                    Text(
+                        text = "TaskyContentBox",
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        )
+    }
+}

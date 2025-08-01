873d08eb46af27afbf0332e564b69ec48906bb1a
arturgie <gubala-artur@wp.pl>
2025-08-01 22:33:29 +0200

    Add hasBeenValidated and isValidPasswordOrNull. To handle focus lose.

--- app/src/main/java/com/example/tasky/auth/domain/PasswordValidationState.kt
+++ app/src/main/java/com/example/tasky/auth/domain/PasswordValidationState.kt
@@ -4,8 +4,12 @@ data class PasswordValidationState(
     val hasMinLength: Boolean = false,
     val hasNumber: Boolean = false,
     val hasLowerCaseCharacter: Boolean = false,
-    val hasUpperCaseCharacter: Boolean = false
+    val hasUpperCaseCharacter: Boolean = false,
+    val hasBeenValidated: Boolean = false
 ) {
     val isValidPassword: Boolean
         get() = hasMinLength && hasNumber && hasLowerCaseCharacter && hasUpperCaseCharacter
+
+    val isValidPasswordOrNull: Boolean?
+        get() = if (hasBeenValidated) isValidPassword else null
 }

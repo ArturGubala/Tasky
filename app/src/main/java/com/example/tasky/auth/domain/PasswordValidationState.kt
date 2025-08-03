bea9d8df393afd397d6586e2a25cec1c27d194f4
arturgie <gubala-artur@wp.pl>
2025-08-03 22:51:36 +0200

    Add state methods which returns list of errors for specific fields.

--- app/src/main/java/com/example/tasky/auth/domain/PasswordValidationState.kt
+++ app/src/main/java/com/example/tasky/auth/domain/PasswordValidationState.kt
@@ -1,5 +1,8 @@
 package com.example.tasky.auth.domain
 
+import com.example.tasky.R
+import com.example.tasky.auth.presentation.register.ValidationItem
+
 data class PasswordValidationState(
     val hasMinLength: Boolean = false,
     val hasNumber: Boolean = false,
@@ -12,4 +15,19 @@ data class PasswordValidationState(
 
     val isValidPasswordOrNull: Boolean?
         get() = if (hasBeenValidated) isValidPassword else null
+
+    fun getPasswordValidationItems(): List<ValidationItem>? {
+        return if (!isValidPassword) {
+            listOf(
+                ValidationItem(
+                    textResId = R.string.at_least_x_characters,
+                    isValid = hasMinLength,
+                    formatArgs = listOf(ValidationRules.MIN_PASSWORD_LENGTH),
+                ),
+                ValidationItem(R.string.at_least_one_number, hasNumber),
+                ValidationItem(R.string.contains_lowercase_char, hasLowerCaseCharacter),
+                ValidationItem(R.string.contains_uppercase_char, hasUpperCaseCharacter)
+            )
+        } else null
+    }
 }
--- app/src/main/java/com/example/tasky/auth/presentation/register/RegisterState.kt
+++ app/src/main/java/com/example/tasky/auth/presentation/register/RegisterState.kt
@@ -1,7 +1,9 @@
 package com.example.tasky.auth.presentation.register
 
-import androidx.compose.foundation.text.input.TextFieldState
+import androidx.annotation.StringRes
+import com.example.tasky.R
 import com.example.tasky.auth.domain.PasswordValidationState
+import com.example.tasky.auth.domain.ValidationRules
 
 data class RegisterState (
     val name: String = "",
@@ -13,8 +15,46 @@ data class RegisterState (
     val passwordValidationState: PasswordValidationState = PasswordValidationState(),
     val isRegistering: Boolean = false,
     val focusedField: FocusedField? = null,
+    val errors: List<ValidationItem>? = null
 )
 
+fun RegisterState.getValidationItemsForFocusedField(): List<ValidationItem>? {
+    return when (focusedField) {
+        FocusedField.NAME -> getNameValidationItems()
+        FocusedField.EMAIL -> getEmailValidationItems()
+        FocusedField.PASSWORD -> passwordValidationState.getPasswordValidationItems()
+        null -> null
+    }
+}
+
+private fun RegisterState.getNameValidationItems(): List<ValidationItem>? {
+    return if (name.isNotEmpty() && isNameValid != null && !isNameValid) {
+        listOf(
+            ValidationItem(
+                textResId = R.string.must_be_a_valid_name,
+                isValid = isNameValid,
+                formatArgs = listOf(ValidationRules.MIN_NAME_LENGTH, ValidationRules.MAX_NAME_LENGTH),
+            )
+        )
+    } else null
+}
+
+private fun RegisterState.getEmailValidationItems(): List<ValidationItem>? {
+    return if (email.isNotEmpty() && isEmailValid != null && !isEmailValid) {
+        listOf(
+            ValidationItem(
+                textResId = R.string.must_be_a_valid_email,
+                isValid = isEmailValid,
+            )
+        )
+    } else null
+}
 enum class FocusedField {
     NAME, EMAIL, PASSWORD
 }
+
+data class ValidationItem(
+    @StringRes val textResId: Int,
+    val isValid: Boolean,
+    val formatArgs: List<Any> = emptyList()
+)

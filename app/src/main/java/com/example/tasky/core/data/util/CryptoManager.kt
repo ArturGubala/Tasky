fae938c2f77f02158179cac15cdf5c95a28e7d59
arturgie <gubala-artur@wp.pl>
2025-08-05 00:12:00 +0200

    Refactor CryptoManager, make functions suspend and main safe by add context Dispatchers.Default.

--- app/src/main/java/com/example/tasky/core/data/util/CryptoManager.kt
+++ app/src/main/java/com/example/tasky/core/data/util/CryptoManager.kt
@@ -2,6 +2,8 @@ package com.example.tasky.core.data.util
 
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import java.security.KeyStore
 import java.util.Base64
@@ -43,15 +45,15 @@ class CryptoManager(private val keyAlias: String = "DefaultEncryptionKey") {
         }.generateKey()
     }
 
-    fun encrypt(data: ByteArray): ByteArray {
+    suspend fun encrypt(data: ByteArray): ByteArray = withContext(Dispatchers.Default) {
         cipher.init(Cipher.ENCRYPT_MODE, getKey())
         val iv = cipher.iv
         val encrypted = cipher.doFinal(data)
-        return iv + encrypted
+        iv + encrypted
     }
 
-    fun decrypt(encryptedData: ByteArray): ByteArray? {
-        return try {
+    suspend fun decrypt(encryptedData: ByteArray): ByteArray? = withContext(Dispatchers.Default) {
+        try {
             val iv = encryptedData.copyOfRange(0, cipher.blockSize)
             val data = encryptedData.copyOfRange(cipher.blockSize, encryptedData.size)
             cipher.init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
@@ -61,12 +63,12 @@ class CryptoManager(private val keyAlias: String = "DefaultEncryptionKey") {
         }
     }
 
-    fun encryptString(plainText: String): String {
+    suspend fun encryptString(plainText: String): String {
         val encryptedBytes = encrypt(plainText.toByteArray())
         return encryptedBytes.encodeToBase64()
     }
 
-    fun decryptString(encryptedBase64: String): String? {
+    suspend fun decryptString(encryptedBase64: String): String? {
         return try {
             val encryptedBytes = encryptedBase64.decodeFromBase64()
             val decryptedBytes = decrypt(encryptedBytes)
@@ -76,12 +78,12 @@ class CryptoManager(private val keyAlias: String = "DefaultEncryptionKey") {
         }
     }
 
-    inline fun <reified T> encryptObject(obj: T, json: Json = Json.Default): String {
+    suspend inline fun <reified T> encryptObject(obj: T, json: Json = Json.Default): String {
         val jsonString = json.encodeToString(obj)
         return encryptString(jsonString)
     }
 
-    inline fun <reified T> decryptObject(encryptedBase64: String, json: Json = Json.Default): T? {
+    suspend inline fun <reified T> decryptObject(encryptedBase64: String, json: Json = Json.Default): T? {
         return try {
             val decryptedJson = decryptString(encryptedBase64)
             decryptedJson?.let { jsonString ->

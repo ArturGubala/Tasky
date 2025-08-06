25eb07f6b956792fe4257c118ef1b15500787671
arturgie <gubala-artur@wp.pl>
2025-08-06 22:48:27 +0200

    Add mainViewModule to MainActivity and and depend length of splash screen show on value isChackingAuth.

--- app/src/main/java/com/example/tasky/app/MainActivity.kt
+++ app/src/main/java/com/example/tasky/app/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.tasky.auth.presentation.register.RegisterScreenRoot
 import com.example.tasky.core.presentation.designsystem.theme.TaskyTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { viewModel.state.value.isCheckingAuth }
+        }
         enableEdgeToEdge()
         // TODO: change to something that change color for system bars to transparent,
         // TODO: have to play with that

7d4748b94bbf74020853d1c889b49d90704824f8
arturgie <gubala-artur@wp.pl>
2025-08-09 22:54:01 +0200

    Extend MainViewModel. Add missing parts, inject authRepository for refreshing access token.

--- app/src/main/java/com/example/tasky/app/MainViewModel.kt
+++ app/src/main/java/com/example/tasky/app/MainViewModel.kt
@@ -2,14 +2,20 @@ package com.example.tasky.app
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.tasky.auth.domain.AuthRepository
 import com.example.tasky.core.data.datastore.EncryptedSessionStorage
+import com.example.tasky.core.domain.AuthState
+import com.example.tasky.core.domain.util.Result
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.format.DateTimeParseException
 
 class MainViewModel(
-    sessionStorage: EncryptedSessionStorage
+    private val sessionStorage: EncryptedSessionStorage,
+    private val authRepository: AuthRepository
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(MainState())
@@ -18,8 +24,52 @@ class MainViewModel(
     init {
         viewModelScope.launch {
             _state.update { it.copy(isCheckingAuth = true) }
-            _state.update { it.copy(isLoggedIn = sessionStorage.get() != null) }
-            _state.update { it.copy(isCheckingAuth = false) }
+
+            val authInfo = sessionStorage.get()
+            val authState = when {
+                authInfo == null -> AuthState.NOT_AUTHENTICATED
+                isTokenExpired(authInfo.accessTokenExpirationTimestamp) -> {
+                    refreshToken(authInfo.refreshToken)
+                }
+                else -> AuthState.AUTHENTICATED
+            }
+
+            _state.update {
+                it.copy(
+                    authState = authState,
+                    isCheckingAuth = false
+                )
+            }
+        }
+    }
+
+    private fun refreshToken(refreshToken: String): AuthState {
+        var resultAuthState = AuthState.TOKEN_EXPIRED
+
+        viewModelScope.launch {
+            val result = authRepository.refreshToken(refreshToken = refreshToken)
+
+            resultAuthState = when(result) {
+                is Result.Error -> {
+                    AuthState.TOKEN_EXPIRED
+                }
+                is Result.Success -> {
+                    AuthState.AUTHENTICATED
+                }
+            }
+        }
+
+        return resultAuthState
+    }
+
+    private fun isTokenExpired(tokenExpirationTimestamp: String): Boolean {
+        return try {
+            val parsedExpirationTimestamp = Instant.parse(tokenExpirationTimestamp)
+            val currentTimestamp = Instant.now()
+
+            currentTimestamp.isAfter(parsedExpirationTimestamp)
+        } catch (_: DateTimeParseException) {
+            true
         }
     }
 }

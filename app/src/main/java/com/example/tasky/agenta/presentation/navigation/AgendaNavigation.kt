b86be1702a088a5fa024353d835d55937ce18002
arturgie <gubala-artur@wp.pl>
2025-08-22 00:19:12 +0200

    Use prepared elements in base AgentaScreen implementation.

--- app/src/main/java/com/example/tasky/agenta/presentation/navigation/AgendaNavigation.kt
+++ app/src/main/java/com/example/tasky/agenta/presentation/navigation/AgendaNavigation.kt
@@ -1,16 +1,11 @@
 package com.example.tasky.agenta.presentation.navigation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import com.example.tasky.agenta.presentation.AgendaScreenRoot
 import kotlinx.serialization.Serializable
 
 fun NavGraphBuilder.agendaNavGraph(
@@ -29,13 +24,7 @@ fun NavGraphBuilder.agendaScreen(
     navController: NavController
 ) {
     composable<AgendaScreen> {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Agenda sccreen")
-        }
+        AgendaScreenRoot()
     }
 }
 

9973ad1eca6c5f77465eb644f4503e3539d4d43c
arturgie <gubala-artur@wp.pl>
2025-09-04 21:50:47 +0200

    Refactor TaskyDatePicker.

---
+++ app/src/main/java/com/example/tasky/core/presentation/designsystem/pickers/TaskyDatePicker.kt
@@ -0,0 +1,143 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.tasky.core.presentation.designsystem.pickers
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.tasky.R
+import com.example.tasky.core.presentation.designsystem.buttons.TaskyTextButton
+import com.example.tasky.core.presentation.designsystem.theme.TaskyTheme
+import com.example.tasky.core.presentation.designsystem.theme.extended
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+@SuppressLint("DefaultLocale")
+@Composable
+fun TaskyDatePicker(
+    datePickerState: DatePickerState,
+    modifier: Modifier = Modifier,
+    isReadOnly: Boolean = false,
+) {
+    var showDatePicker by remember { mutableStateOf(false) }
+    val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH)
+    var selectedDate = datePickerState.selectedDateMillis?.let { dateFormat.format(Date(it)) }
+
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(color = MaterialTheme.colorScheme.extended.surfaceHigher)
+            .clickable(enabled = !isReadOnly) {
+                showDatePicker = true
+            }
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = selectedDate ?: stringResource(R.string.nothing_selected),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .weight(1f)
+            )
+
+            if (!isReadOnly) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = stringResource(R.string.select_date),
+                    tint = Color.Gray,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+        }
+    }
+
+    if (showDatePicker && !isReadOnly) {
+        DatePickerDialog(
+            onDismissRequest = { showDatePicker = false },
+            confirmButton = {
+                TaskyTextButton(
+                    onClick = {
+                        showDatePicker = false
+                    },
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 4.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.confirm)
+                    )
+                }
+            },
+            dismissButton = {
+                TaskyTextButton(
+                    onClick = { showDatePicker = false },
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 4.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.cancel)
+                    )
+                }
+            }
+        ) {
+            DatePicker(
+                state = datePickerState,
+                title = {
+                    Text(
+                        text = stringResource(R.string.choose_date),
+                        modifier = Modifier.padding(start = 24.dp, top = 16.dp)
+                    )
+                }
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun TaskyDatePickerPreview() {
+    TaskyTheme {
+        TaskyDatePicker(
+            datePickerState = DatePickerState(locale = Locale.ENGLISH, initialSelectedDateMillis = System.currentTimeMillis()),
+            isReadOnly = false,
+            modifier = Modifier
+                .width(150.dp)
+        )
+    }
+}

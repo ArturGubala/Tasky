93994c777359e63af4dfecf1628de725fe7e8359
arturgie <gubala-artur@wp.pl>
2025-09-07 15:33:57 +0200

    Keep only hours in time part while initialize timestamp AgendaDetailState field.

--- app/src/main/java/com/example/tasky/agenda/presentation/agenda_detail/AgendaDetailState.kt
+++ app/src/main/java/com/example/tasky/agenda/presentation/agenda_detail/AgendaDetailState.kt
@@ -7,12 +7,13 @@ import com.example.tasky.agenda.presentation.util.AgendaReminderInterval
 import com.example.tasky.agenda.presentation.util.AgendaDetailConfigProvider
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
 
 data class AgendaDetailState(
     val loadingInitialData: Boolean = false,
     val selectedAgendaReminderInterval: AgendaReminderInterval = AgendaDetailConfigProvider
         .getDefaultReminderIntervals().first(),
-    val timestamp: ZonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"))
+    val timestamp: ZonedDateTime = ZonedDateTime.now(ZoneId.of("UTC")).truncatedTo(ChronoUnit.HOURS)
 ) {
     val localTimestamp: ZonedDateTime
         get() = timestamp.withZoneSameInstant(ZoneId.systemDefault())

648c579b426bd4df7bd3cd971438d150cd43c2f1
arturgie <gubala-artur@wp.pl>
2025-09-10 23:29:05 +0200

    Create TaskyAttendeeStatusRadioButton and AgendaItemAttendeesStatus enum with options for it.

---
+++ app/src/main/java/com/example/tasky/agenda/presentation/util/AgendaItemAttendeesStatus.kt
@@ -0,0 +1,9 @@
+package com.example.tasky.agenda.presentation.util
+
+enum class AgendaItemAttendeesStatus(
+    val displayName: String,
+) {
+    ALL("All"),
+    GOING("Going"),
+    NOT_GOING("Not going")
+}

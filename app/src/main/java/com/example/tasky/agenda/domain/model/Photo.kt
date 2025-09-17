73bf715d83b24e05cffdf61dd9d36ddb372e7ae2
arturgie <gubala-artur@wp.pl>
2025-09-17 21:06:45 +0200

    Split Photo to domain and presentation models. Refactor AgendaPhotoPicker and rename it to RememberAgendaPhotoPickerLauncher.

--- app/src/main/java/com/example/tasky/agenda/domain/model/Photo.kt
+++ app/src/main/java/com/example/tasky/agenda/domain/model/Photo.kt
@@ -1,8 +1,7 @@
 package com.example.tasky.agenda.domain.model
 
-import android.net.Uri
-
 data class Photo(
     val id: String,
-    val uri: Uri,
+    val uri: String,
+    val compressedBytes: ByteArray? = null
 )

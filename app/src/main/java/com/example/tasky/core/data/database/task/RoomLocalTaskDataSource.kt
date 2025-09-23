52abc7679cd656d823f5e971f3890d526ca890e7
arturgie <gubala-artur@wp.pl>
2025-09-23 22:23:06 +0200

    Create TaskLocalDataStore interface and implement it in RoomLocalTaskDataSource.

---
+++ app/src/main/java/com/example/tasky/core/data/database/task/RoomLocalTaskDataSource.kt
@@ -0,0 +1,47 @@
+package com.example.tasky.core.data.database.task
+
+import android.database.sqlite.SQLiteFullException
+import com.example.tasky.agenda.domain.model.Task
+import com.example.tasky.core.data.database.task.dao.TaskDao
+import com.example.tasky.core.data.database.task.mappers.toTask
+import com.example.tasky.core.data.database.task.mappers.toTaskEntity
+import com.example.tasky.core.domain.data.TaskLocalDataStore
+import com.example.tasky.core.domain.util.DataError
+import com.example.tasky.core.domain.util.EmptyResult
+import com.example.tasky.core.domain.util.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class RoomLocalTaskDataSource(
+    private val taskDao: TaskDao,
+) : TaskLocalDataStore {
+
+    override fun getTask(id: String): Flow<Task> {
+        return taskDao.getTask(id = id)
+            .map { it.toTask() }
+    }
+
+    override fun getTasksForDay(
+        startOfDay: Long,
+        endOfDay: Long,
+    ): Flow<List<Task>> {
+        return taskDao.getTasksForDay(startOfDay = startOfDay, endOfDay = endOfDay)
+            .map { taskEntities ->
+                taskEntities.map { it.toTask() }
+            }
+    }
+
+    override suspend fun upsertTask(task: Task): EmptyResult<DataError.Local> {
+        return try {
+            val entity = task.toTaskEntity()
+            taskDao.upsertTask(entity)
+            Result.Success(Unit)
+        } catch (_: SQLiteFullException) {
+            Result.Error(DataError.Local.DISK_FULL)
+        }
+    }
+
+    override suspend fun deleteTask(id: String) {
+        taskDao.deleteTask(id)
+    }
+}

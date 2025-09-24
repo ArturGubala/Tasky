c0d0254269dfe541d6c44cd12c5a2fe9d4e66f12
arturgie <gubala-artur@wp.pl>
2025-09-24 19:42:39 +0200

    Create TaskRepository and implement it in OfflineFirstTaskRepository.

--- app/src/main/java/com/example/tasky/agenda/data/di/AgendaDataModule.kt
+++ app/src/main/java/com/example/tasky/agenda/data/di/AgendaDataModule.kt
@@ -1,11 +1,14 @@
 package com.example.tasky.agenda.data.di
 
 import com.example.tasky.agenda.data.network.task.KtorTaskDataSource
-import com.example.tasky.agenda.domain.TaskRemoteDataSource
+import com.example.tasky.agenda.data.repository.OfflineFirstTaskRepository
+import com.example.tasky.agenda.domain.data.TaskRepository
+import com.example.tasky.agenda.domain.data.network.TaskRemoteDataSource
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val agendaNetworkModule = module {
     singleOf(::KtorTaskDataSource).bind<TaskRemoteDataSource>()
+    singleOf(::OfflineFirstTaskRepository).bind<TaskRepository>()
 }
---
+++ app/src/main/java/com/example/tasky/agenda/data/repository/OfflineFirstTaskRepository.kt
@@ -0,0 +1,43 @@
+package com.example.tasky.agenda.data.repository
+
+import com.example.tasky.agenda.domain.data.TaskRepository
+import com.example.tasky.agenda.domain.data.network.TaskRemoteDataSource
+import com.example.tasky.agenda.domain.model.Task
+import com.example.tasky.core.domain.data.TaskLocalDataStore
+import com.example.tasky.core.domain.util.DataError
+import com.example.tasky.core.domain.util.EmptyResult
+import com.example.tasky.core.domain.util.Result
+import com.example.tasky.core.domain.util.asEmptyDataResult
+
+class OfflineFirstTaskRepository(
+    private val taskRemoteDataSource: TaskRemoteDataSource,
+    private val taskLocalDataStore: TaskLocalDataStore,
+) : TaskRepository {
+
+    override suspend fun createTask(task: Task): EmptyResult<DataError> {
+        val localResult = taskLocalDataStore.upsertTask(task)
+        if (localResult !is Result.Success) {
+            return localResult.asEmptyDataResult()
+        }
+
+        val remoteResult = taskRemoteDataSource.createTask(task)
+        return when (remoteResult) {
+            is Result.Error -> {
+                when (remoteResult.error) {
+                    DataError.Network.CONFLICT -> {
+                        return remoteResult.asEmptyDataResult()
+                    }
+
+                    else -> {
+                        // TODO write task to sync queue
+                    }
+                }
+                Result.Success(Unit)
+            }
+
+            is Result.Success -> {
+                remoteResult.asEmptyDataResult()
+            }
+        }
+    }
+}

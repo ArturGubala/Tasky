434c8379c911ac95fe31b703e7f0c5f71eea8da0
arturgie <gubala-artur@wp.pl>
2025-09-28 13:34:12 +0200

    Add EventRepository and implement it.

--- app/src/main/java/com/example/tasky/agenda/data/di/AgendaDataModule.kt
+++ app/src/main/java/com/example/tasky/agenda/data/di/AgendaDataModule.kt
@@ -4,6 +4,7 @@ import com.example.tasky.agenda.data.network.event.KtorEventDataSource
 import com.example.tasky.agenda.data.network.reminder.KtorReminderDataSource
 import com.example.tasky.agenda.data.network.task.KtorTaskDataSource
 import com.example.tasky.agenda.data.network.tasky.KtorTaskyDataSource
+import com.example.tasky.agenda.data.repository.OfflineFirstEventRepository
 import com.example.tasky.agenda.data.repository.OfflineFirstReminderRepository
 import com.example.tasky.agenda.data.repository.OfflineFirstTaskRepository
 import com.example.tasky.agenda.data.repository.TaskyRepositoryImpl
@@ -11,6 +12,7 @@ import com.example.tasky.agenda.data.sync.DeleteAgendaItemWorker
 import com.example.tasky.agenda.data.sync.FetchAgendaItemsWorker
 import com.example.tasky.agenda.data.sync.SyncAgendaItemWorkerScheduler
 import com.example.tasky.agenda.data.sync.UpsertAgendaItemWorker
+import com.example.tasky.agenda.domain.data.EventRepository
 import com.example.tasky.agenda.domain.data.ReminderRepository
 import com.example.tasky.agenda.domain.data.TaskRepository
 import com.example.tasky.agenda.domain.data.TaskyRepository
@@ -29,8 +31,10 @@ val agendaNetworkModule = module {
     singleOf(::KtorReminderDataSource).bind<ReminderRemoteDataSource>()
     singleOf(::KtorEventDataSource).bind<EventRemoteDataSource>()
     singleOf(::KtorTaskyDataSource).bind<TaskyRemoteDataSource>()
+
     singleOf(::OfflineFirstTaskRepository).bind<TaskRepository>()
     singleOf(::OfflineFirstReminderRepository).bind<ReminderRepository>()
+    singleOf(::OfflineFirstEventRepository).bind<EventRepository>()
     singleOf(::TaskyRepositoryImpl).bind<TaskyRepository>()
 
     workerOf(::UpsertAgendaItemWorker)
---
+++ app/src/main/java/com/example/tasky/agenda/data/repository/OfflineFirstEventRepository.kt
@@ -0,0 +1,149 @@
+package com.example.tasky.agenda.data.repository
+
+import com.example.tasky.agenda.domain.data.EventRepository
+import com.example.tasky.agenda.domain.data.network.EventRemoteDataSource
+import com.example.tasky.agenda.domain.data.sync.SyncAgendaItemScheduler
+import com.example.tasky.agenda.domain.model.AgendaItem
+import com.example.tasky.agenda.domain.model.Event
+import com.example.tasky.core.data.database.SyncOperation
+import com.example.tasky.core.data.database.event.dao.EventPendingSyncDao
+import com.example.tasky.core.data.database.event.mappers.toEvent
+import com.example.tasky.core.domain.data.EventLocalDataSource
+import com.example.tasky.core.domain.datastore.SessionStorage
+import com.example.tasky.core.domain.util.DataError
+import com.example.tasky.core.domain.util.EmptyResult
+import com.example.tasky.core.domain.util.Result
+import com.example.tasky.core.domain.util.asEmptyDataResult
+import com.example.tasky.core.domain.util.onError
+import com.example.tasky.core.domain.util.onSuccess
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class OfflineFirstEventRepository(
+    private val eventRemoteDataSource: EventRemoteDataSource,
+    private val eventLocalDataSource: EventLocalDataSource,
+    private val applicationScope: CoroutineScope,
+    private val eventPendingSyncDao: EventPendingSyncDao,
+    private val sessionStorage: SessionStorage,
+    private val syncAgendaItemScheduler: SyncAgendaItemScheduler,
+) : EventRepository {
+
+    override suspend fun upsertEvent(
+        event: Event,
+        syncOperation: SyncOperation,
+    ): EmptyResult<DataError> {
+        val localResult = eventLocalDataSource.upsertEvent(event)
+        if (localResult !is Result.Success) {
+            return localResult.asEmptyDataResult()
+        }
+
+        when (syncOperation) {
+            SyncOperation.CREATE -> {
+                return eventRemoteDataSource.createEvent(event)
+                    .onError { error ->
+                        applicationScope.launch {
+                            syncAgendaItemScheduler.scheduleSync(
+                                type = SyncAgendaItemScheduler.SyncType.UpsertAgendaItem(
+                                    item = AgendaItem.Event(id = event.id),
+                                    operation = syncOperation
+                                )
+                            )
+                        }.join()
+                    }.asEmptyDataResult()
+            }
+
+            SyncOperation.UPDATE -> {
+                return eventRemoteDataSource.updateEvent(event)
+                    .onError { error ->
+                        applicationScope.launch {
+                            syncAgendaItemScheduler.scheduleSync(
+                                type = SyncAgendaItemScheduler.SyncType.UpsertAgendaItem(
+                                    item = AgendaItem.Event(id = event.id),
+                                    operation = syncOperation
+                                )
+                            )
+                        }.join()
+                    }.asEmptyDataResult()
+            }
+        }
+    }
+
+    override suspend fun deleteEvent(id: String): EmptyResult<DataError> {
+        eventLocalDataSource.deleteEvent(id = id)
+
+        return eventRemoteDataSource.deleteEvent(id = id)
+            .onError { error ->
+                applicationScope.launch {
+                    syncAgendaItemScheduler.scheduleSync(
+                        type = SyncAgendaItemScheduler.SyncType.DeleteAgendaItem(
+                            item = AgendaItem.Event(id = id)
+                        )
+                    )
+                }.join()
+            }.asEmptyDataResult()
+    }
+
+    override suspend fun syncPendingEvent() {
+        withContext(Dispatchers.IO) {
+            val userId = sessionStorage.get()?.userId ?: return@withContext
+
+            val createdEvents = async {
+                eventPendingSyncDao.getAllEventPendingSyncEntities(userId)
+            }
+            val deletedEvents = async {
+                eventPendingSyncDao.getAllDeletedEventSyncEntities(userId)
+            }
+
+            val createJobs = createdEvents
+                .await()
+                .map { it ->
+                    launch {
+                        val event = it.event.toEvent()
+                        when (it.operation) {
+                            SyncOperation.CREATE -> {
+                                eventRemoteDataSource.createEvent(event)
+                                    .onSuccess {
+                                        applicationScope.launch {
+                                            eventPendingSyncDao.deleteEventPendingSyncEntity(
+                                                eventId = it.id,
+                                                operations = listOf(SyncOperation.CREATE)
+                                            )
+                                        }.join()
+                                    }
+                            }
+
+                            SyncOperation.UPDATE -> {
+                                eventRemoteDataSource.updateEvent(event)
+                                    .onSuccess {
+                                        applicationScope.launch {
+                                            eventPendingSyncDao.deleteEventPendingSyncEntity(
+                                                eventId = it.id,
+                                                operations = listOf(SyncOperation.UPDATE)
+                                            )
+                                        }.join()
+                                    }
+                            }
+                        }
+                    }
+                }
+            val deleteJobs = deletedEvents
+                .await()
+                .map {
+                    launch {
+                        eventRemoteDataSource.deleteEvent(it.eventId)
+                            .onSuccess { result ->
+                                applicationScope.launch {
+                                    eventPendingSyncDao.deleteDeletedEventSyncEntity(it.eventId)
+                                }.join()
+                            }
+                    }
+                }
+
+            createJobs.forEach { it.join() }
+            deleteJobs.forEach { it.join() }
+        }
+    }
+}
